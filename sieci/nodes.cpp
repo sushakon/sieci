99d42a7d448b656b9388aaf8a02b013528991645
Agata Slonka <draon88@gmail.com>
2022-01-07 09:00:14 +0100

    nie wiem co robie, ale to nic nowego

--- sieci/nodes.cpp
+++ sieci/nodes.cpp
@@ -5,11 +5,42 @@
 
 void ReceiverPreferences::add_receiver(IPackageReceiver* r){
 
+//    int size = preferences_.size();
+//    size ++;
+//    double step = 1/size;
+//
+//    for (auto it : preferences_) {
+//
+//        it.second = step;
+//        step += step;
+//    }
+
 };
+
 void ReceiverPreferences::remove_receiver(IPackageReceiver* r){
+
+    double multiplier = 1/(1 - preferences_[r]);
+    preferences_.erase(r);
+
+    for (auto it = begin(); it != end(); ++it){
+
+        preferences_[it->first] = it->second * multiplier;
+    }
+
 
 };
 IPackageReceiver* ReceiverPreferences::choose_receiver(){
 
+    IPackageReceiver* receiver;
+    double sum = cbegin()->second;
+
+    for (auto it = begin(); it != end(); ++it){
+
+        if (probability_ < sum){
+
+        }
+    }
+
+    return receiver;
 
 };
--- sieci/nodes.hpp
+++ sieci/nodes.hpp
@@ -6,7 +6,10 @@
 #define SIECI_NODES_HPP
 
 #include <map>
+#include <memory>
 #include "types.hpp"
+#include "package.hpp"
+#include "storage_types.hpp"
 
 
 enum class ReceiverType {
@@ -19,51 +22,87 @@ enum class ReceiverType {
 
 class IPackageReceiver
 {
-
+    public:
+        virtual void receive_package(Package&& p);
+        virtual ElementID get_id();
 };
 
-class Storehouse : IPackageReceiver{
+class Storehouse : public IPackageReceiver{
 
+    public:
+
+        Storehouse(ElementID id, std::unique_ptr<IPackageStockpile> d);
 };
 
 class ReceiverPreferences{
 
-public:
-    ReceiverPreferences(ProbabilityGenerator pg){probability_ = pg();};
+    public:
+        ReceiverPreferences() = default;
+        ReceiverPreferences(ProbabilityGenerator pg){probability_ = pg();};
 
-    using preferences_t = std::map<IPackageReceiver*, double>;
-    using const_iterator = preferences_t::const_iterator;
+        using preferences_t = std::map<IPackageReceiver*, double>;
+        using const_iterator = preferences_t::const_iterator;
 
-    const_iterator cbegin() const { return preferences_.cbegin(); }
-    const_iterator cend() const { return preferences_.cend(); }
-    const_iterator begin() const { return preferences_.begin(); }
-    const_iterator end() const { return preferences_.end(); }
+        const_iterator cbegin() const { return preferences_.cbegin(); }
+        const_iterator cend() const { return preferences_.cend(); }
+        const_iterator begin() const { return preferences_.begin(); }
+        const_iterator end() const { return preferences_.end(); }
 
-    void add_receiver(IPackageReceiver* r);
-    void remove_receiver(IPackageReceiver* r);
-    IPackageReceiver* choose_receiver();
-    preferences_t& get_preferences(){return preferences_;};
+        void add_receiver(IPackageReceiver* r);
+        void remove_receiver(IPackageReceiver* r);
+        IPackageReceiver* choose_receiver();
+        preferences_t& get_preferences(){return preferences_;};
 
 
-    preferences_t preferences_ ;
+        preferences_t preferences_ ;
 
-private:
-    double probability_ = 0;
+    private:
+        double probability_ = 0;
 
 
 };
 
-class PackageSender: ReceiverPreferences{
+class PackageSender: public ReceiverPreferences{
+
+    public:
+
+        PackageSender() = default;
+        PackageSender(PackageSender&&) = default;
+
+        void send_package();
+        std::optional<Package>& get_sending_buffer();
+
+        ReceiverPreferences receiver_preferences_;
+
+    protected:
+        void push_package(Package&& p);
+
 };
 
 class Ramp: PackageSender
 {
+    public:
+
+        Ramp(ElementID id, TimeOffset di): id_(id), di_(di){};
+        void deliver_goods(Time t);
+        TimeOffset get_delivery_interval();
+        ElementID get_id();
+
+    private:
+        ElementID id_;
+        TimeOffset di_;
 
 };
 
-class Worker : PackageSender, IPackageReceiver
+class Worker : public PackageSender, public IPackageReceiver
 {
+    public:
+        Worker(ElementID id, TimeOffset pd, std::unique_ptr<IPackageQueue> q);
 
+        void do_work(Time t);
+
+        TimeOffset get_processing_duration();
+        Time get_package_processing_start_time();
 };
 
 
