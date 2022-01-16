70809c5e03af30d5727c6fe5759c911a77f44680
sushakon <the.death.of.fish@gmail.com>
2022-01-16 18:24:13 +0100

    what is wrong xd

--- net_sim-main/include/reports.hpp
+++ net_sim-main/include/reports.hpp
@@ -2,8 +2,8 @@
 // Created by Agata on 04.01.2022.
 //
 
-#ifndef NETSIM_REPORTS_HPP
-#define NETSIM_REPORTS_HPP
+#ifndef REPORTS_HPP
+#define REPORTS_HPP
 
 #include "nodes.hpp"
 #include "factory.hpp"
@@ -13,4 +13,4 @@ void generate_structure_report(Factory& f, std::ostream& os);
 void generate_simulation_turn_report(Factory& f, std::ostream& os, Time t);
 
 
-#endif //NETSIM_REPORTS_HPP
+#endif //REPORTS_HPP
